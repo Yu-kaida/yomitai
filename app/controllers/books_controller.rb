6b2c84e8f1c335591eb6bb2e6ee9dc423a112e1c
Yu-kaida <moondrip4869@gmail.com>
2023-09-13 17:46:12 +0900

    moromoro

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
   def index
+    if session[:user_id]
+	    # findの場合はユーザを存在しないとエラーを返すのでfind_byを使う
+      @user = User.find_by(id: session[:user_id])
+    end
     @books = Book.all
   end
 end
